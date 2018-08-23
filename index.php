6783a78f48501eda8ca448d8274663882562cae6
Nhat Huynh Van <jonirenoug@gmail.com>
2018-08-23 20:41:36 +0700

    Ok bayby

--- index.php
+++ index.php
@@ -3,11 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>PHP Application Renoug ABCF</title>
+    <title>PHP Application Renoug</title>
     <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
-    <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
-    <link rel="shortcut icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
     <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     <link rel="stylesheet" href="/styles.css" type="text/css">
 </head>
